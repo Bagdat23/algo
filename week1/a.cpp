0de05af74ef46099f3a5d9f5b2d9fa23267f4936
Ramzik <ramzik2022@mail.ru>
2021-11-11 03:47:21 +0600

    lab1-a.cpp

--- week1/a.cpp
+++ week1/a.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
 using namespace std;
+struct st{
+	int kol[100];
+	int cnt = 0;
+	void push(long long x){
+		kol[cnt++] = x;
+	}
+	void pop(){
+		if(cnt == 0){
+			cout << "error\n";
+		}
+		else{
+			cout << kol[cnt-1] << endl;
+			kol[cnt-1]=100;
+			cnt--;
+		}
+	}
+	void back(){
+		if(cnt == 0){
+			cout << "error\n";
+		}
+		else{
+			cout << kol[cnt-1] << endl;
+		}
+	}
+	void size(){
+		cout << cnt << endl;
+	}
+	void clear(){
+		for(int i=0;i<cnt;++i)
+			kol[cnt] = 100;
+		cnt = 0;
+		cout << "ok\n";
+
+	}
+};
 int main(){
-    int a;
-    cin >> a;
-    cout << a;
+	st xxx;
+	string sss;
+	long long nnn;
+	while(cin >> sss){
+		if(sss == "exit"){
+			cout << "bye";
+			break;}
+		if(sss=="push"){
+			cin >> nnn;
+			xxx.push(nnn);
+			cout << "ok\n";
+		}
+		if(sss=="size") xxx.size();
+		if(sss=="pop") xxx.pop();
+		if(sss=="back") xxx.back();
+		if(sss=="clear") xxx.clear();
+	}
 }
